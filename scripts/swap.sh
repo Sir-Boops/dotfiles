62de3d16e78329f7b13578b3503d87614c8619a3
Sir_Boops <admin@boops.me>
2018-01-01 12:17:52 -0700

    Fix swap math

--- scripts/swap.sh
+++ scripts/swap.sh
@@ -1,5 +1,5 @@
 SWAP=`cat /proc/swaps`
 USED=`echo ${SWAP} | awk -F"[ ]" '/Filename/{print $9}'`
 SIZE=`echo ${SWAP} | awk -F"[ ]" '/Filename/{print $8}'`
-PER=`echo "scale=1; (${USED}/${SIZE}) * 100" | bc`
-echo "<span font='FontAwesome'>&#xf2db; SWP: ${PER}%</span>"
+PER=`echo "scale=10; (${USED}/${SIZE}) * 100" | bc`
+echo "<span font='FontAwesome'>&#xf2db; SWP: ${PER%.*}%</span>"
