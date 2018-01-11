e39f849859f89c8e0a45c35620845fcdd398c5b3
Sir_Boops <admin@boops.me>
2018-01-11 09:10:04 -0700

    Update icons

--- scripts/cpu.sh
+++ scripts/cpu.sh
@@ -1,2 +1,2 @@
 PER=`top -bn 2 -d 0.5 | grep '^%Cpu' | tail -n 1 | gawk '{print $2+$4+$6}'`
-echo `bash ~/.dotfiles/scripts/color_per.sh ${PER%.*} '&#xf2db; CPU:'`
+echo `bash ~/.dotfiles/scripts/color_per.sh ${PER%.*} '&#xf085; CPU:'`
--- scripts/ram.sh
+++ scripts/ram.sh
@@ -3,4 +3,4 @@ TOTAL=`echo ${RAM} | awk -F"[ ]" '/MemTotal:/{print $2}'`
 FREE=`echo ${RAM} | awk -F"[ ]" '/MemTotal:/{print $5}'`
 USED=`echo "scale=10; ${TOTAL} - ${FREE}" | bc`
 PER=`echo "scale=10; (${USED}/${TOTAL}) * 100" | bc`
-echo `bash ~/.dotfiles/scripts/color_per.sh ${PER%.*} '&#xf2db; MEM:'`
+echo `bash ~/.dotfiles/scripts/color_per.sh ${PER%.*} '&#xf1c0; MEM:'`
